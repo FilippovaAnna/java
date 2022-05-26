f07053b6c20a30973adbaf7b80c93e1527becfd7
Anna Filippova <a.filippova@phkp.ru>
2022-05-26 17:54:33 +0300

    lesson3

---
+++ ForTesters/src/main/java/lesson3/Main.java
@@ -0,0 +1,23 @@
+package lesson3;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Box<Apple> appleBox = new Box<>();
+        appleBox.addFruit(new Apple(1));
+        appleBox.addFruit(new Apple(1));
+        appleBox.addFruit(new Apple(1));
+        System.out.println(appleBox.getWeight());
+
+        Box<Apple> appleBox2 = new Box<>();
+        appleBox.addFruit(new Apple(1));
+        appleBox.addFruit(new Apple(1));
+
+
+        appleBox.pourOver(appleBox2);
+        System.out.println(appleBox.getFruitList());
+        System.out.println(appleBox2.getFruitList());
+
+    }
+}
+
