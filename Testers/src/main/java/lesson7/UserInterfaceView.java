fb750011a6600012f4cba965d449537aa7a41df9
Anna Filippova <a.filippova@phkp.ru>
2022-07-29 16:49:08 +0300

    New project webuijava

--- Testers/src/main/java/lesson7/UserInterfaceView.java
+++ Testers/src/main/java/lesson7/UserInterfaceView.java
@@ -27,4 +27,9 @@ public class UserInterfaceView {
             }
         }
     }
+
+    public static void main(String[] args) {
+        UserInterfaceView view = new UserInterfaceView();
+        view.runInterface();
+    }
 }
